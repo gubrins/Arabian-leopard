5848b044573eac78f30034c6e6106d23f50f7ef0
gubrins <32134625+gubrins@users.noreply.github.com>
2021-10-27 16:27:11 +0200

    Update make_windows.sh

--- phylogeny/make_windows.sh
+++ phylogeny/make_windows.sh
@@ -1,2 +1,2 @@
 #you need to have the size for each chromosome of your genome:
-bedtools makewindows -g sizes.genome -w 500000 > 500K.txt 
+bedtools makewindows -g sizes.genome -w 1000000 > 1MB.txt 
