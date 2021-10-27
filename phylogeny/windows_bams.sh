28d63d3a86653272388dbbd8e2c9b3c989917bf8
gubrins <32134625+gubrins@users.noreply.github.com>
2021-10-27 16:37:00 +0200

    Create windows_bams.sh

---
+++ phylogeny/windows_bams.sh
@@ -0,0 +1,16 @@
+samples="/home/panthera/gabri/leopards/analisis/autosomes/phylogeny/samples.txt"
+location_bams="/mnt/DiscoB/leopards/bams/"
+windows="
+
+for sample in $(cat samples);
+do
+for window in 
+samtools view -b "$location""$sample"
+
+
+
+
+
+for i in $(cat complete_windows.txt); do samtools view -b ../arabian_leopard_1_catmapped_mkdup_30.bam $i > $i.bam ;done
+
+
