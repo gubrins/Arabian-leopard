f3181f3bff15cd55ab5dfe772d236faea5eb71d8
gubrins <32134625+gubrins@users.noreply.github.com>
2022-02-25 16:17:21 +0100

    Update 2_psmc.sh

--- analyses/psmc/2_psmc.sh
+++ analyses/psmc/2_psmc.sh
@@ -1,2 +1,2 @@
-#-q20 is the quality filtering. This is by default, but we previously filtered per both quality and mapping quality.
+#-q20 is the quality filtering. This is by default, but we previously filtered per 30 both quality and mapping quality.
 /home/panthera/software/psmc/utils/fq2psmcfa -q20 diploid.fq.gz > diploid.psmcfa
