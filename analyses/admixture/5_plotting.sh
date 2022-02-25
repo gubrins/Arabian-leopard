3ba381e16f6f545dcaf41588ac213b1cbf709ccb
gubrins <32134625+gubrins@users.noreply.github.com>
2022-02-25 16:07:06 +0100

    Create 5_plotting.sh

---
+++ analyses/admixture/5_plotting.sh
@@ -0,0 +1,3 @@
+Rscript plotADMIXTURE_edited.R -p qc_randsnp_cerastes_50gen_50ind_hard_filtering0_17_usnps12 \
+-i qc_randsnp_cerastes_50gen_50ind_hard_filtering0_17_usnps12.txt -k 8 -m 1 \
+-l Cerastes.cerastes,Cerastes.vipera,Cerastes.gasperetti -t TreeOrder2.txt 
