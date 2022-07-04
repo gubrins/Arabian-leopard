a71386d54da8bc04bc9e0420706c82c04b39dd6a
gubrins <32134625+gubrins@users.noreply.github.com>
2022-07-04 12:40:06 +0200

    Update count_variants.sh

--- analyses/snpEff/count_variants.sh
+++ analyses/snpEff/count_variants.sh
@@ -6,3 +6,48 @@ cat output.ann.vcf \
 | tr "," "\n" \
 | grep MODIFIER \
 | wc -l
+
+
+
+for i in $(bcftools query -l max_missing1_all_leopards.vcf.gz); 
+do
+cd $i
+echo $i 
+#zcat $i.vcf.gz |grep -v "#" |wc -lecho "0_0"
+zcat "$i"_results_0_0.vcf.gz\
+    | cut -f 8 \
+    | grep -v "#" \
+    | tr ";" "\n" \
+    | grep ^ANN= \
+    | cut -f 2 -d = \
+    | tr "," "\n" \
+    | grep MODERATE \
+    | wc -l
+echo ""
+
+echo "1_1"
+zcat "$i"_results_1_1.vcf.gz\
+    | cut -f 8 \
+    | grep -v "#" \
+    | tr ";" "\n" \
+    | grep ^ANN= \
+    | cut -f 2 -d = \
+    | tr "," "\n" \
+    | grep MODERATE \
+    | wc -l
+echo ""
+echo "0_1"
+zcat "$i"_results_0_1.vcf.gz\
+    | cut -f 8 \
+    | grep -v "#" \
+    | tr ";" "\n" \
+    | grep ^ANN= \
+    | cut -f 2 -d = \
+    | tr "," "\n" \
+    | grep MODERATE \
+    | wc -l
+echo ""
+echo ""
+
+cd ..
+done
