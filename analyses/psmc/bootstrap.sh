b5d515c80c754c2c95b68669be9c9382275b8775
gubrins <32134625+gubrins@users.noreply.github.com>
2022-07-19 12:29:01 +0200

    Update bootstrap.sh

--- analyses/psmc/bootstrap.sh
+++ analyses/psmc/bootstrap.sh
@@ -1,3 +1,8 @@
+#First of all, we need to split our diploid file:
+utils/splitfa diploid.psmcfa > split.psmcfa
+
+
+#we generate as many bootstraps as we want:
 for i in $(seq 1 10);
 do
 /home/panthera/software/psmc/psmc -N25 -t15 -r5 -b -p "4+25*2+4+6" -o round-"$i".psmc split.psmcfa
@@ -6,8 +11,8 @@ done
 
 #then, we have to combine all these files called round (which will be the bootstrapping) with the original PSMC of the individual:
 
-cat round* diploid.psmc > combined_bootstrap.psmc
+cat round* diploid.psmcfa > combined_bootstrap.psmc
 
-#and then we plot it:
+#and finally we plot it:
 
 psmc_plot.pl -u 1.1e-8 -g 5 output combined_boostrap.psmc
