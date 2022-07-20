e013bff4a82d2292cc8f2a6b1682ed18b26dacf0
gubrins <32134625+gubrins@users.noreply.github.com>
2022-07-20 17:53:42 +0200

    Update 1_get_fq.sh

--- analyses/psmc/1_get_fq.sh
+++ analyses/psmc/1_get_fq.sh
@@ -1,2 +1,2 @@
-#First of all, you convert the bam file into fq. IMPORTANT, set -d and -D to half and double the averaged coverage of your sample!
+#First of all, you convert the bam file into fq. IMPORTANT, set -d and -D to a third and double the averaged coverage of your sample!
 samtools mpileup -C50 -uf reference_genome.fna file.bam |bcftools call -c -  |vcfutils.pl vcf2fq -d 3 -D 30 |gzip > diploid.fq.gz
