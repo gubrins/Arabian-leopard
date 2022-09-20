3d3e814bdae926ca35c9a14a362fabcef803b345
adtalave <66261868+adtalave@users.noreply.github.com>
2022-09-20 13:25:27 +0200

    Update script.sh

--- analyses/heterozygosity/script.sh
+++ analyses/heterozygosity/script.sh
@@ -9,8 +9,8 @@ callable=$(tabix -h $vcf $region | vcftools --gzvcf - --indv $sample --remove-in
 		value=${region#*:}
 		start=$(echo "$value" |cut -d- -f1)
 		chrom=$(echo "$region" |cut -d: -f1)
-		tabix -h $vcf $region | vcftools --gzvcf - --indv $sample --remove-indels --max-alleles 2 --minQ 30 --minDP 2 --maxDP 300 --stdout --recode --recode-INFO-all | vcfhetcount | tail -n 1 | awk -v l="$callable" -v chrom="$chrom" -v start="$start" '{print chrom"\t"start+50000"\t"$1"\t"l"\t"$1/l}'
+		tabix -h $vcf $region | vcftools --gzvcf - --indv $sample --remove-indels --max-alleles 2 --minQ 30 --minDP 2 --maxDP 300 --stdout --recode --recode-INFO-all | vcfhetcount | tail -n 1 | awk -v l="$callable" -v chrom="$chrom" -v start="$start" '{print chrom"\t"start+50000"\t"$1"\t"l"\t"$1/l}' | tee -a "$sample"_heterozygosity.txt
 	else
-		echo $line | awk -v l=0 -v het=NA '{print $1"\t"$2+50000"\t"l"\t"l"\t"het}'
+		echo $line | awk -v l=0 -v het=NA '{print $1"\t"$2+50000"\t"l"\t"l"\t"het}' | tee -a "$sample"_heterozygosity.txt
 	fi
 done
