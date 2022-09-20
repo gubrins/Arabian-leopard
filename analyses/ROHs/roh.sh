0cd2ad38424a6b5e62629e3b6612563e9839cb55
gubrins <32134625+gubrins@users.noreply.github.com>
2022-09-20 12:07:40 +0200

    Update roh.sh

--- analyses/ROHs/roh.sh
+++ analyses/ROHs/roh.sh
@@ -1,1 +1,7 @@
+for i in $(bcftools query -l just_leopards_gatk.vcf.gz_09.vcf.gz);
+do
 
+(VCF=just_leopards_gatk.vcf.gz_09.vcf.gz
+vcftools --gzvcf $VCF --indv $i --recode --stdout |bcftools roh --AF-dflt 0.4 > 09/results_"$i".txt) &
+done
+wait
