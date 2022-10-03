81154d8e7ee5a9c2edadfc3a44cf764374b256d2
gubrins <32134625+gubrins@users.noreply.github.com>
2022-10-03 15:12:20 +0200

    Update script_repetitive.sh

--- processing_data/filter_vcf/script_repetitive.sh
+++ processing_data/filter_vcf/script_repetitive.sh
@@ -1,1 +1,3 @@
 cat $fasta | python scan_characters.py actg | bedtools merge -i - > lower_case_positions.bed
+
+bedtools intersect -v -a file.vcf -b lower_case_positions.bed |bgzip -c > filtered_file.vcf.gz
