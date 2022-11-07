7485fa0bcd1a2bfc1660ad90a849d67357feb9fb
adtalave <66261868+adtalave@users.noreply.github.com>
2022-11-07 15:17:00 +0100

    Update filter_branches_high_missing_data.sh

--- analyses/phylogeny_new2/filter_branches_high_missing_data.sh
+++ analyses/phylogeny_new2/filter_branches_high_missing_data.sh
@@ -1,6 +1,6 @@
 #you need to count the number of Ns per sliding windows per individual:
 
-/home/panthera/software/UCSC/faCount sliding_windows.txt |perl -ane 'next if(/\#/); print "$F[0]\t$F[1]\t$F[6]\t".($F[6]/($F[1]-$F[6]))."\n";'
+/home/panthera/software/UCSC/faCount sliding_windows.txt |perl -ane 'next if(/\#/); print "$F[0]\t$F[1]\t$F[6]\t".($F[6]/($F[1]-$F[6]))."\n";' | tee -a all.txt
 
 #and then, you open this with R and you just keep the ones with a proportion of missingness higher than 0.49:
 
