c67b315188f1df4dfeb210cc70fabf27a78924a4
adtalave <66261868+adtalave@users.noreply.github.com>
2022-11-07 16:50:22 +0100

    Update filter_branches_high_missing_data.sh

--- analyses/phylogeny_new2/filter_branches_high_missing_data.sh
+++ analyses/phylogeny_new2/filter_branches_high_missing_data.sh
@@ -5,4 +5,22 @@
 #and then, you open this with R and you just keep the ones with a proportion of missingness higher than 0.49:
 
 data = read.table("all.txt")
-data = data[data$V4>0.49,]
+windows_wHighMiss = data[data$V4>0.49,]
+
+#again in linux, to keep unique windows without sample names:
+
+sed 's/^.*sliding_//g' windows_wHighMiss.txt > windows_wHighMiss_nosamplenames.txt
+sort windows_wHighMiss_nosamplenames.txt | uniq -w 20 | tee -a uniq_windows.txt
+cat uniq_windows.txt | wc -l #to know how many do not pass the threshold
+
+#we remove those windows before concatenating the trees
+
+for undesired_window in $(cat uniq_windows.txt);
+do
+rm new_iqtree_vipera_:"$undesired_window".txt.treefile
+done
+
+
+
+
+
