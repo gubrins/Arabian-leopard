36b28c76e437025438fdab0b97f95664260f375f
gubrins <32134625+gubrins@users.noreply.github.com>
2024-05-15 11:40:02 +0200

    Update 5_plotting.sh

--- analyses/admixture/5_plotting.sh
+++ analyses/admixture/5_plotting.sh
@@ -1,3 +1,3 @@
 Rscript plotADMIXTURE_edited.R -p output_name \
 -i output_name.txt -k 8 -m 1 \
--l Cerastes.cerastes,Cerastes.vipera,Cerastes.gasperetti -t TreeOrder.txt 
+-l arabian_leopard,asian_leopard -t TreeOrder.txt 
